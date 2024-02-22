59316994ff87a7936e22a809b95d414470dd130e
alejo190404 <alejo190404@gmail.com>
2024-02-22 00:02:07 -0500

    Actualización: Cambio de filas implementado

--- gauss.py
+++ gauss.py
@@ -1,3 +1,5 @@
+from math import factorial
+
 #Función para imprimir matriz en consola
 def imprimirMatriz(matriz):
     print("Matriz:")
@@ -20,6 +22,31 @@ def transformarFila(matriz, Fpivot, Ftransform, factor):
     for i in range(len(matriz) + 1):
         matriz[Ftransform][i] = matriz[Ftransform][i] - (matriz[Fpivot][i] * factor)
 
+#Función para aplicar algorimto de Gauss
+def gauss(matrix, n):
+    for i in range(n):
+        if (matrix[i][i]==0):
+            return i #Devuelve el número de iteración en la cual se encontró el pivote 0, para que otra función realice el cambio de fila
+        for j in range(n-i-1):
+            factor = matrix[j+i+1][i]/matrix[i][i]
+            transformarFila(matrix, i, j+i+1, factor)
+        imprimirMatriz(matrix)
+    return -1 #Devuelve -1 si en esta iteración de Gauss, no se requirió realizar más cambios de fila
+
+#Función para cambiar filas dentro de la matriz
+
+def cambiarFilasMatriz(matriz, Fproblema):
+    n = len(matriz)
+    if (n == Fproblema):
+        print("El sistema de ecuaciones no se pudo resolver. Asegúrese que esté bien escrito y que las ecuaciones tengan independencia lineal")
+        imprimirMatriz(matriz)
+        exit()
+    else:
+        aux = matriz[Fproblema]
+        matriz[Fproblema] = matriz[Fproblema + 1]
+        matriz[Fproblema + 1] = aux
+        return matriz
+
 
 
 #Comienzo del programa
@@ -39,15 +66,19 @@ for i in range(n):
     matrix.append(row)
 
 #Aplicar algoritmo de Gauss
-imprimirMatriz(matrix)
 for i in range(n):
-    if (matrix[i][i]==0):
-        print("La matriz tiene un pivote en 0. Asegúrese que el primer elemnto no sea 0, que las ecuaciones tengan independencia lienal o ingrese la matriz en otro orden")
-        exit()
-    for j in range(n-i-1):
-        factor = matrix[j+i+1][i]/matrix[i][i]
-        transformarFila(matrix, i, j+i+1, factor)
-    imprimirMatriz(matrix)
+    problema = gauss(matrix, n)
+    if (problema != -1):
+        matrix = cambiarFilasMatriz(matrix, problema)
+        imprimirMatriz(matrix)
+    else:
+        break
+
+
+
+print("Resultado final: ")
+imprimirMatriz(matrix)
+
 
 #Despejar ecuaciones reempleazando "hacia atrás"
 soluciones =[]
